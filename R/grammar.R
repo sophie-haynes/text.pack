ed541a71e67e22d0433d4582e4b33806bed7305d
Sophie Haynes <thesophienator96@gmail.com>
2018-01-22 02:05:37 +0000

    Bug Fix: Separate Corpus When Conv. to Data Table

--- R/grammar.R
+++ R/grammar.R
@@ -17,54 +17,51 @@
 #' grammar(text.corpus)
 
 grammar <- function(corpus){
-  # convert corpus to data table object
-  text.table <- data.table(
-    text = sapply(corpus, paste, collapse = " ")
+    # convert corpus to data table object
+    text.table <- data.table(
+        text = sapply(corpus, as.character)
     )
-  #Loop through each line
-for(i in 1:nrow(text.table)){
-    tryCatch({
-        #Tokenise line
-        words <- as.vector(strsplit(as.character(text.table[i,])," ")[[1]])
-        #Treetag each word
-        tagged <- suppressWarnings(treetag(
-            format="obj",
-            words, 
-            treetagger="manual",
-            lang="en", 
-            TT.options=list(
-                path="~/TreeTagger",
-                preset="en")                    
-        ))
-        table = taggedText(tagged)
-        table$new = NA
-        for(j in 1:nrow(table)){
-            if(table[j,]$lemma == "<unknown>"){
-                table[j,]$new = table[j,]$token
-            } else{
-                table[j,]$new = table[j,]$lemma
+    colnames(text.table) = c("text")
+    #Loop through each line
+    for(i in 1:nrow(text.table)){
+        tryCatch({
+            #Tokenise line
+            words <- as.vector(strsplit(as.character(
+                text.table[i,])," ")[[1]])
+            #Treetag each word
+            tagged <- suppressWarnings(treetag(
+                format="obj", words, 
+                treetagger="manual", lang="en", 
+                TT.options=list(
+                    path="~/TreeTagger",
+                    preset="en")
+            ))
+            table = taggedText(tagged)
+            table$new = NA
+            for(j in 1:nrow(table)){
+                if(table[j,]$lemma == "<unknown>"){
+                    table[j,]$new = table[j,]$token
+                } else{
+                    table[j,]$new = table[j,]$lemma
+                }
             }
-        }
-
-        #Make new string from cleaned text
-        string <- paste(c(table$new),collapse=" ")
-    }, 
-    warning = function(w){
-        print("Warning")
-        print(i)
-        print(paste("\nMY_WARNING:  ",w))
-    }, 
-    error = function(e){
-        print("Oh no! I had an error!")
-        print(i)
-        print(paste(e))
-        string = words
-
-    },
-    finally = {
-        text.table$clean_treetag[i] = string
-    })
-}
-  text.table
-  #ßreturn(text.table)
+            #Make new string from cleaned text
+            string <- paste(c(table$new),collapse=" ")
+        }, 
+        warning = function(w){
+            print("Warning")
+            print(i)
+            print(paste("\nMY_WARNING:  ",w))
+        }, 
+        error = function(e){
+            print("Oh no! I had an error!")
+            print(i)
+            print(paste(e))
+            string = words
+        },
+        finally = {
+            text.table$text[i] = string
+        })
+    }
+    text.table
 }
